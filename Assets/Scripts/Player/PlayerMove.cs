4a711f29bfd162b3b359d83ee4ec4eeffe1330ac
sadflask <benjamin.n.frew@gmail.com>
2017-07-21 23:30:31 +1200

    Initial player movement.

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    //-1 0 1
+    private int lane;
+
+    private Vector3 startPosition;
+    private Vector3 targetPosition;
+
+    void Awake()
+    {
+        startPosition = transform.position;
+        targetPosition = startPosition;
+    }
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            //Move left
+            lane = (lane == -1) ? -1 : lane - 1;
+            targetPosition = startPosition + lane * Vector3.right * 0.3f;
+        } else if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            //Move right
+            lane = (lane == 1) ? 1 : lane + 1;
+            targetPosition = startPosition + lane * Vector3.right * 0.3f;
+        } else if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            //Jump thing
+        } else if (Input.GetKeyUp(KeyCode.PageDown))
+        {
+            //Slide thing
+        }
+        //Lerp
+        transform.position = Vector3.Lerp(transform.position,targetPosition,0.25f);
+	}
+
+    
+}
