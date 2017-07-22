18cc8a8c3e57db1814b58a96ee5dd1de73268474
Saren Currie <saren@sarencurrie.com>
2017-07-22 15:48:41 +1200

    Add chasing knight gameobjects

---
+++ Assets/Scripts/Knight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : MonoBehaviour {
+
+	public float speed = 0;
+	private Rigidbody rb;
+
+	public void Awake()
+	{
+		// rb = GetComponent<Rigidbody>();
+		// speed = 0.5f;
+	}
+
+	public void FixedUpdate()
+	{
+		// var realSpeed = this.speed - road.speed;
+		// rb.position += Vector3.forward * realSpeed;
+	}
+}
